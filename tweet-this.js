a2f3534c542ddce367f5d6bfe419db8f65666c8c
Christian Heilmann <chris.heilmann@gmail.com>
2023-04-06 14:41:36 +0200

    Update tweet-this.js

--- tweet-this.js
+++ tweet-this.js
@@ -1,14 +1,16 @@
 (function(){
-    n=getSelection().anchorNode;
-    if(!n) {
-        t = document.title;
-    } else {
-        t = n.nodeType === 3 ? n.data : n.innerText;
-    }
-    t = '“' + t.trim() + '”\n\n';
-    window.open(
-        `https://twitter.com/intent/tweet?text=
-        ${encodeURIComponent(t)}
-        ${document.location.href}`
-    )
+let d = document;
+let t = d.title;
+let u = d.location.href;
+var s = window.getSelection().getRangeAt(0).cloneContents();
+var tw = d.createTreeWalker(s, NodeFilter.SHOW_TEXT);
+var c = '';
+while (tw.nextNode()) { 
+    c = c.concat(treeWalker.currentNode.nodeValue);
+}
+let out = encodeURIComponent(`👉🏼 „${t}”\n🔗${u}\n💬 ${c}`);
+console.log(`[${t}](${u})\n${c}`);
+navigator.clipboard.writeText(snippet);
+window.open(`https://twitter.com/intent/tweet?text=${out}`);
 })();
+
